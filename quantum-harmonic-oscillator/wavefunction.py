b221ff30849afbdf6b28cee5b61cac52ed7037cf
Ross Mcphee <rossmcphee82@gmail.com>
2024-10-04 02:11:52 +0100

    Now also momentum icluded

--- quantum-harmonic-oscillator/wavefunction.py
+++ quantum-harmonic-oscillator/wavefunction.py
@@ -20,3 +20,26 @@ def expectation_value(n, x_vals):
     expectation_value2 = np.trapz(x_vals**2 * prob_density, x_vals)
     std_deviation = np.sqrt(expectation_value2 - expectation_value**2)
     return expectation_value, std_deviation
+
+# we also know that there is a momentum 'wavefunction' representation
+
+#phi(k) = 1/sqrt(2pihbar) I(psi(x)*exp(-ipx/hbar)dx, so we need to use a fft 
+def momentum_wavefunction(n, p_vals):
+    # Generate a range of x values
+    x_vals = np.linspace(-10, 10, 500)
+    psi_n_x = wavefunction(n, x_vals)
+    
+    # Fourier transform to momentum space
+    psi_n_p = np.fft.fftshift(np.fft.fft(psi_n_x))
+    p_space = np.fft.fftshift(np.fft.fftfreq(x_vals.size, d=(x_vals[1] - x_vals[0])))
+    
+    # Interpolate the result to match p_vals
+    return np.interp(p_vals, p_space, np.abs(psi_n_p))
+
+def momentum_expectation_value(n, p_vals):
+    psi_n_p = momentum_wavefunction(n, p_vals)
+    probability_density_p = np.abs(psi_n_p)**2
+    expectation_p = np.trapz(p_vals * probability_density_p, p_vals)
+    expectation_p2 = np.trapz(p_vals**2 * probability_density_p, p_vals)
+    uncertainty_p = np.sqrt(expectation_p2 - expectation_p**2)
+    return expectation_p, uncertainty_p
